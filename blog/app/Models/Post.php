cf1bde94362a001c8dc1d6ff3e1a986163ef0d5a
saidak-vlad <74212237+saidak-vlad@users.noreply.github.com>
2021-09-17 00:43:47 +0300

    +methods for admin

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
 
 class Post extends Model
 {
@@ -12,10 +15,10 @@ class Post extends Model
 
     protected $fillable = ['title', 'content'];
 
-   public function category()
-   {
-       return $this->hasOne(Category::class);
-   }
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
 
     public function author()
     {
@@ -63,9 +66,82 @@ class Post extends Model
 
     }
 
+    public function uploadImage($image)
+    {
+        if($image == null){ return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = Str::random(10) . ' . ' .  $image->extension();
+        $image->saveAs('uploads', '$filename');
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getImage()
+    {
+        if($this->image == null)
+        {
+            return '/img/no-image.png';
+        }
+        return '/uploads/' . $this->image;
+    }
+
+    public  function setCategory($id)
+    {
+        if($id == null) {return;}
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    public function setTags($ids)
+    {
+        if($ids == null){return;}
+
+        $this->tags()->sync($ids);
+    }
 
 
+    public function setDraft()
+    {
+        $this->status = 0;
+        $this->save();
+    }
 
+    public function setPublic()
+    {
+        $this->status = 1;
+        $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+        if($value == null)
+        {
+            return $this->setDraft();
+        }
+        return $this->setPublic();
+    }
+
+    public function setFeatured()
+    {
+        $this->status = 1;
+        $this->save();
+    }
+
+    public function setStandart()
+    {
+        $this->status = 0;
+        $this->save();
+    }
+
+    public function toggleFeatured($value)
+    {
+        if($value == null)
+        {
+            return $this->setStandart();
+        }
+        return $this->setFeatured();
+    }
 
 }
 
