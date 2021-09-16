d530da849c8f0aad13e66ea4864d5420cc5af4de
saidak-vlad <74212237+saidak-vlad@users.noreply.github.com>
2021-09-17 01:14:58 +0300

    add metods for user

--- blog/app/Models/User.php
+++ blog/app/Models/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -47,8 +49,95 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
-    public function comments    ()
+    public function comments()
     {
         return $this->hasMany(Comment::class);
     }
+
+    public static function add($fields)
+    {
+        $user = new static;
+        $user->fill($fields);
+        $user->password = bcrypt($fields['password']);
+        $user->save();
+
+        return $user;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->password = bcrypt($fields['password']);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        Storage::delete('uploads/' . $this->image);
+        $this->delete();
+    }
+
+    public function uploadAvatar($image)
+    {
+        if($image == null){ return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = Str::random(10) . ' . ' .  $image->extension();
+        $image->saveAs('uploads', '$filename');
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getImage()
+    {
+        if($this->image == null)
+        {
+            return '/img/no-user-image.png';
+        }
+        return '/uploads/' . $this->image;
+    }
+
+    public function makeAdmin()
+    {
+        $this->is_admin = 1;
+        $this->save();
+    }
+
+    public function makeNormal()
+    {
+        $this->is_admin = 0;
+        $this->save();
+    }
+
+    public function toggleAdmin($value)
+    {
+        if($value == null)
+        {
+            return $this->makeNormal();
+        }
+        return $this->makeAdmin();
+    }
+
+    public function ban()
+    {
+        $this->status = 1;
+        $this->save();
+    }
+
+    public function unban()
+    {
+        $this->status = 0;
+        $this->save();
+    }
+
+    public function toggleBun($value)
+    {
+        if($value == null)
+        {
+            return $this->unbun();
+        }
+        return $this->bun();
+    }
+
+
 }
