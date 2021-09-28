a0a7b2aee7aece882d63f8e42b38b8db9f905b49
saidak-vlad <74212237+saidak-vlad@users.noreply.github.com>
2021-09-28 14:31:47 +0300

    created register and login

--- blog/app/Models/User.php
+++ blog/app/Models/User.php
@@ -71,6 +71,15 @@ class User extends Authenticatable
         $this->save();
     }
 
+    public function generatePassword($password)
+    {
+        if($password != null)
+        {
+            $this->password = bcrypt($password);
+            $this->save();
+        }
+    }
+
     public function remove()
     {
         Storage::delete('uploads/' . $this->image);
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -18,6 +18,12 @@ Route::get('/post/{slug}', 'App\Http\Controllers\HomeController@show')->name('po
 Route::get('/tag/{slug}', 'App\Http\Controllers\HomeController@tag')->name('tag.show');
 Route::get('/category/{slug}', 'App\Http\Controllers\HomeController@category')->name('category.show');
 
+Route::get('/register', 'App\Http\Controllers\AuthController@registerForm');
+Route::post('/register', 'App\Http\Controllers\AuthController@register');
+Route::get('/login', 'App\Http\Controllers\AuthController@loginForm');
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+
+Route::get('/logout', 'App\Http\Controllers\AuthController@logout');
 
 Route::get('/admin' , 'App\Http\Controllers\Admin\DashboardController@index');
 Route::resource('/admin/categories' , 'App\Http\Controllers\Admin\CategoriesController');
