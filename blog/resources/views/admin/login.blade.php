54cf1251a9832203833cb00850afc4d2e9fe1b10
saidak-vlad <74212237+saidak-vlad@users.noreply.github.com>
2021-10-05 13:46:51 +0300

    done

---
+++ blog/resources/views/admin/login.blade.php
@@ -0,0 +1,47 @@
+@extends('layout')
+
+@section('content')
+    <!--main content start-->
+    <div class="main-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+
+                    <div class="leave-comment mr0"><!--leave comment-->
+                        @if(session('status'))
+                            {{session('status')}}
+                        @endif
+                        <h3 class="text-uppercase">Login</h3>
+
+                        <br>
+                        <form class="form-horizontal contact-form" role="form" method="post" action="/admin.login">
+                            {{csrf_field()}}
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" id="email" name="email"
+                                           placeholder="Email" value="{{old('email')}}">
+                                    @error('email')
+                                    <p class="text-danger">{{$message}}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <input type="password" class="form-control" id="password" name="password"
+                                           placeholder="password">
+                                    @error('password')
+                                    <p class="text-danger">{{$message}}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                            <button type="submit" class="btn send-btn">Login</button>
+
+                        </form>
+                    </div><!--end leave comment-->
+                </div>
+                @include('pages.sidebar')
+            </div>
+        </div>
+    </div>
+    <!-- end main content-->
+@endsection
